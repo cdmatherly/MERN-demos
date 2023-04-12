14bd79b31862044af329a8134147070c8291dc3f
Chase <chsmatherly@gmail.com>
2023-04-12 08:03:48 -0700

    solved algo

--- algorithms/src/arrays/diagonalDifference/index.js
+++ algorithms/src/arrays/diagonalDifference/index.js
@@ -6,38 +6,53 @@
 */
 
 const squareMatrix1 = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [9, 8, 9],
-];
-const expected1 = 2;
-/* 
-  left to right diagonal: 1 + 5 + 9 = 15
-  right to left diagonal: 3 + 5 + 9 = 17
-  absolute difference = 2
-*/
+    [1, 2, 3],
+    [4, 5, 6],
+    [9, 8, 9],
+  ];
+  const expected1 = 2;
+  /* 
+    left to right diagonal: 1 + 5 + 9 = 15
+    right to left diagonal: 3 + 5 + 9 = 17
+    absolute difference = 2
+  */
+  
+  const squareMatrix2 = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+  ];
+  const expected2 = 0;
+  /* 
+    left to right diagonal: 1 + 2 + 3 + 4 + 5 = 15
+    right to left diagonal: 5 + 4 + 3 + 2 + 1 = 15
+    absolute difference = 0
+  */
+  
+  /**
+   * Calculates the absolute diagonal difference of a square matrix.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Array<Array<number>>} sqrMatrix A 2d array of numbers representing
+   *    a square matrix (rows and columns).
+   * @returns {number} Represents the absolute difference between the top left to
+   *    bottom right diagonal and the top right to bottom left diagonal.
+   */
+function diagonalDifference(sqrMatrix) {
+    let sum1 = sqrMatrix[0][0];
+    let sum2 = sqrMatrix[0][sqrMatrix[0].length-1];
 
-const squareMatrix2 = [
-  [1, 2, 3, 4, 5],
-  [1, 2, 3, 4, 5],
-  [1, 2, 3, 4, 5],
-  [1, 2, 3, 4, 5],
-  [1, 2, 3, 4, 5],
-];
-const expected2 = 0;
-/* 
-  left to right diagonal: 1 + 2 + 3 + 4 + 5 = 15
-  right to left diagonal: 5 + 4 + 3 + 2 + 1 = 15
-  absolute difference = 0
-*/
+    for(let i = 1; i < sqrMatrix.length; i++){
+        sum1 += sqrMatrix[i][i];
+        sum2 += sqrMatrix[i][sqrMatrix.length - (i+1)];
+    }
 
-/**
- * Calculates the absolute diagonal difference of a square matrix.
- * - Time: O(?).
- * - Space: O(?).
- * @param {Array<Array<number>>} sqrMatrix A 2d array of numbers representing
- *    a square matrix (rows and columns).
- * @returns {number} Represents the absolute difference between the top left to
- *    bottom right diagonal and the top right to bottom left diagonal.
- */
-function diagonalDifference(sqrMatrix) {}
+    const difference = sum1 - sum2
+
+    return Math.abs(difference)
+}
+
+    console.log(diagonalDifference(squareMatrix1));
+    console.log(diagonalDifference(squareMatrix2));
