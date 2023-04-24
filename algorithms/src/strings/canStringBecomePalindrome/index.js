b01dc6b4f3fce094f7112d3f63d10393b1047646
Chase <chsmatherly@gmail.com>
2023-04-24 09:24:51 -0700

    solved algo

--- algorithms/src/strings/canStringBecomePalindrome/index.js
+++ algorithms/src/strings/canStringBecomePalindrome/index.js
@@ -31,4 +31,45 @@ What is it about a string that makes it POSSIBLE for it to become a palindrome i
  * @param {string} str
  * @returns {boolean} Whether the given str can be rearranged into a palindrome.
  */
-function canStringBecomePalindrome(str) {}
+function canStringBecomePalindrome(str) {
+    //first check if string has at least one character
+    if(str.length > 0){ 
+        const occurances = {};
+        //loop through 
+        for(let i = 0; i < str.length; i++){
+            if(occurances.hasOwnProperty(str[i])){
+                occurances[str[i]]++
+            } else {
+                occurances[str[i]] = 1;
+            }
+        }
+        if(str.length %2 == 0){
+            const values = Object.values(occurances);
+            for(let j = 0; j < values.length; j++){
+                if(values[j] %2 != 0){
+                    return false;
+                }
+            }
+        } else {
+            const values = Object.values(occurances);
+            let foundOdd = false;
+            for(let j = 0; j < values.length; j++){
+                if(values[j] %2 != 0){
+                    if(foundOdd == true){
+                        return false;
+                    }
+                    foundOdd = true;
+                }
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+console.log(canStringBecomePalindrome(str1));
+console.log(canStringBecomePalindrome(str2));
+console.log(canStringBecomePalindrome(str3));
+console.log(canStringBecomePalindrome(str4));
+console.log(canStringBecomePalindrome(str5));
+console.log(canStringBecomePalindrome(str6));
